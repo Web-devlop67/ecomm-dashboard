c338c4d6003ff8123c792fbdf091a5d16cb9794e
Web-devlop67 <114478279+Web-devlop67@users.noreply.github.com>
2022-09-27 22:21:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+
+  return(
+    <div className="App">
+    <h1>E-Comm Project</h1>
+    <Button> Button</Button>
+    </div>
+
+  )
+ 
+}
+
+export default App;
